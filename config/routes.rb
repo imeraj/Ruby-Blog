d4d4515de1e9a5b14921319be9518509a0e11152
Mohammad Merajul Islam Molla <merajul.molla@sekai-lab-bd.com>
2017-02-06 16:28:18 +0600

    API versioning code more formatted
    Front-end broken now
    
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,22 @@ require 'api_constraints'
 Rails.application.routes.draw do
   get 'welcome/index'
 
-    scope module: :v1, constraints: APIConstraints.new(version: 1, defualt: true), defaults: { format: 'json' } do
-      resources :articles do
-        resources :comments
-      end
+  def api_version(version, &routes)
+    api_constraints = APIConstraints.new(version: version)
+    scope module: "v#{version}", constraints: api_constraints, defaults: { format: 'json' }, &routes
+  end
+
+  api_version(1) do
+    resources :articles do
+      resources :comments
     end
+  end
+
+  api_version(2) do
+    resources :articles do
+      resources :comments
+    end
+  end
 
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- lib/api_constraints.rb
+++ lib/api_constraints.rb
@@ -3,11 +3,10 @@ class APIConstraints
 
   def initialize(options)
     @version = options[:version]
-    @default = options[:default]
   end
 
   def matches?(request)
-    @default || request.headers[:accept].include?("version=#{@version}")
+    request.headers[:accept].include?("version=#{@version}")
   end
 
 end
