c985d269499b5390c9d3dbf7747690683b7a973a
Mohammad Merajul Islam Molla <merajul.molla@sekai-lab-bd.com>
2017-02-07 09:47:36 +0600

    Now supports two forms of versioning - URL based and Accept header based. See comments in routes.rb file
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -3,21 +3,32 @@ require 'api_constraints'
 Rails.application.routes.draw do
   get 'welcome/index'
 
-    def api_version(version, &routes)
-      api_constraints = APIConstraints.new(version: version)
-      scope module: "api/v#{version}", constraints: api_constraints, defaults: { format: :json }, &routes
-    end
+  def api_version(version, &routes)
+    api_constraints = APIConstraints.new(version: version)
+
+    # comment out below line and uncommment namespace lines below to use url based versioning mechanism,
+    # right now its header based
+    scope module: "api/v#{version}", constraints: api_constraints, defaults: { format: :json }, &routes
+  end
 
   api_version(1) do
-    resources :articles do
-      resources :comments
-    end
+    # namespace :api do
+    #  namespace :v1 do
+        resources :articles do
+          resources :comments
+        end
+    #  end
+    # end
   end
 
   api_version(2) do
-    resources :articles do
-      resources :comments
-    end
+   # namespace :api do
+   #   namespace :v2 do
+        resources :articles do
+          resources :comments
+        end
+   #   end
+   # end
   end
 
   root 'welcome#index'
